ba115227bc9c964cbbfaddf4e366eb36e65d14cf
Shirotsune <forgetgaming@gmail.com>
2015-07-31 16:32:05 +0300

    object manager, Check!

--- src/HopeToFly/Main.java
+++ src/HopeToFly/Main.java
@@ -11,6 +11,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Initialize the game by loading one by one the assets.
+        ObjectManager Objects = new ObjectManager(); // Responsible of all objects.
+        RendingManager GUI = new RendingManager(); // Responsible of drawing all game assets.
+        SoundManager Sound = new SoundManager(); // Responsible of the sounds.
+        EventHandler Listener = new EventHandler(); // Responsible of the controls.
+        
+        // Start game loop.
         for(;;){
             
             
--- src/HopeToFly/ObjectManager.java
+++ src/HopeToFly/ObjectManager.java
@@ -5,7 +5,31 @@ Copyright (c) 2015 - All rights reserved.
  */
 
 package HopeToFly;
+import HopeToFly.Objects.*;
+import java.util.ArrayList;
 
 public class ObjectManager {
+    
+    PlayerObject Player;
+    ArrayList<WishObject> Wishes;
+    ArrayList<StarObject> Stars;
+    ArrayList<FearObject> Fears;
+    
+    public ObjectManager(){
+        this.Player = new PlayerObject();
+        this.Wishes = new ArrayList<WishObject>();
+        this.Stars = new ArrayList<StarObject>();
+        this.Fears = new ArrayList<FearObject>();
+        
+        // We have to do with 8 objects of each type. To help with performance.
+        // Although stars and wishesh are linked I'd like to keep the two completely
+        // seperated from one another.
+        for(int i = 0; i<8; i++)
+        {
+        Wishes.add(new WishObject());
+        Stars.add(new StarObject());
+        Fears.add(new FearObject());
+        }
+    }
     
 }
